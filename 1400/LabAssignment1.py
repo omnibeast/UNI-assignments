e47e0bd69ecee655401c6155d848cc6e67bb0fd2
Saurav Pokharel <78597546+omnibeast@users.noreply.github.com>
2024-09-03 09:25:46 -0700

    Update LabAssignment1.py

---
+++ 1400/LabAssignment1.py
@@ -0,0 +1,9 @@
+"""
+Programer: Saurav Pokharel
+Date: 09/03/2024
+Description:Generates a Poem based on user input. 
+"""
+what = input("How are you feeling?")
+want = input("What do you want?")
+print('I am', what)
+print('I want', want)
