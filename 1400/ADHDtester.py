4a03c8199d9809f1f22ba00b5e850e471f096e24
Saurav Pokharel <78597546+omnibeast@users.noreply.github.com>
2024-09-03 19:48:31 -0700

    ADHD Tester

---
+++ 1400/ADHDtester.py
@@ -0,0 +1,24 @@
+"""
+Programer: Saurav Pokharel
+Date: 09/02/2024
+Description:ADHD Tester. 
+"""
+point = 0 #default point set to 0
+print('Rate the following activities on a scale from 1 to 5 on how your child does with the following activities on the scale of 1-5, 5 being the very well and 1 being not well at all.')
+
+organize = int (input('How well do you manage your time on a scale of 1-5? '))
+point += organize
+
+homework = int(input('How punctual are you with homework deadlines on a scale of 1-5? '))
+point += homework
+
+tasks = int(input('How well do you manage sequential tasks on a scale of 1-5? '))
+point += tasks
+
+room = int(input('How well do you keep your room organized on a scale of 1-5? '))
+point += room
+
+detail = int(input('On a scale of 1-5, how would you rate your attention to detail? '))
+point += detail
+
+print('You have scored total points of', point)
--- 1400/LabAssignment1.py
+++ 1400/LabAssignment1.py
@@ -5,5 +5,9 @@ Description:Generates a Poem based on user input.
 """
 what = input("How are you feeling?")
 want = input("What do you want?")
+worries = input("What do you want me to worry about you?")
+luckyno = input("What's your lucky number?")
+understand = input ("What do you understand?")
+
 print('I am', what)
 print('I want', want)
