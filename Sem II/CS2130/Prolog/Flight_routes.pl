e44dde879aa7c17f5bbb154e1fbcc6aececcaed8
Saurav Pokharel <78597546+omnibeast@users.noreply.github.com>
2025-01-26 17:12:59 -0700

    prolog 2

---
+++ Sem II/CS2130/Prolog/Flight_routes.pl
@@ -0,0 +1,33 @@
+% Saurav Pokharel
+% CS2130
+% Prolog program to determine airline routes between cities, 
+
+%--- FACTS ---
+% Direct flights between cities
+flight(dgz, qyy).
+flight(dgz, azi).
+flight(qyy, csi).
+flight(azi, tva).
+flight(csi, ppg).
+flight(tva, brw).
+flight(brw, csi).
+
+%--- RULES ---
+% Direct flight with 0 stops
+route(Origin, Destination, 0):- flight(Origin, Destination).
+
+% Add one stop to route
+route(Origin, Destination, Stops) :-
+    flight(Origin, Intermediate), route(Intermediate, Destination, Stops1), Stops is Stops1 + 1.
+
+% route cities list
+route_with_cities(Origin, Destination, Stops, [Origin|Cities]) :-  
+    flight(Origin, Destination), 
+    Stops is 0, 
+    Cities = [Destination].
+route_with_cities(Origin, Destination, Stops, [Origin|Cities]) :- 
+    flight(Origin, Intermediate), 
+    route_with_cities(Intermediate, Destination, Stops1, Cities1), 
+    Stops is Stops1 + 1, 
+    Cities = [Intermediate|Cities1].
+
